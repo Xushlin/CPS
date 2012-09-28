a78d6d6bb07f937a7274bd21b710a2f62bb11c95
Micheal <vaiyanzi@gmail.com>
2012-09-28 17:58:10 +0800

    Add base model and Employee model

---
+++ GitUseDemo.Web/GitUseDemo.ViewModel/BaseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GitUseDemo.ViewModel
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ GitUseDemo.Web/GitUseDemo.ViewModel/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GitUseDemo.ViewModel
+{
+    public class Employee:BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
