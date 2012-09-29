e5a1e603ceb03207d391d07ff8f016da5ae6f5c5
Micheal <vaiyanzi@gmail.com>
2012-09-29 09:47:20 +0800

    delete basemodel.cs and add CagegoryViewModel.cs

--- GitUseDemo.Web/GitUseDemo.ViewModel/BaseModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GitUseDemo.ViewModel
-{
-    public abstract class BaseModel
-    {
-        public int Id { get; set; }
-    }
-}
---
+++ GitUseDemo.Web/GitUseDemo.ViewModel/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GitUseDemo.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
