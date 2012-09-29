a7c234c7fca00d660a84321be27e7565a9a56440
Micheal <vaiyanzi@gmail.com>
2012-09-29 09:19:40 +0800

    add Data project in Solution

---
+++ GitUseDemo.Web/GitUseDemo.Data/Northwind.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\xushiling\Documents\GitHub\CPS\GitUseDemo.Web\GitUseDemo.Data\Northwind.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
